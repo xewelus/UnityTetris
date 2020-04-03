a5b805f0b03b3700063ede33412c8b9cc20a0db3
dinar <dinar.mobile@gmail.com>
2020-04-03 20:56:22 +0300

    стенка, позиция камеры, тестовое создание массива

--- Assets/GameSystem.cs
+++ Assets/GameSystem.cs
@@ -7,10 +7,21 @@ public class GameSystem : MonoBehaviour
 	private GameSystem m_Instance;
 	public GameSystem Instance { get { return this.m_Instance; } }
 
+	public GameObject AtomCube;
+
 	// ReSharper disable once UnusedMember.Local
 	void Awake()
 	{
 		this.m_Instance = this;
+
+		
+		for (int y = 0; y < 20; y++)
+		{
+			for (int x = 0; x < 10; x++)
+			{
+				Instantiate(this.AtomCube, new Vector3(x, y, 0), Quaternion.identity);
+			}
+		}
 	}
 
 	// ReSharper disable once UnusedMember.Local
