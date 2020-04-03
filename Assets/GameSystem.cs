d72ece119a7d2a524a4537307fd874f8f5482c37
dinar <dinar.mobile@gmail.com>
2020-04-03 20:08:02 +0300

    GameSystem

---
+++ Assets/GameSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSystem : MonoBehaviour
+{
+	private GameSystem m_Instance;
+	public GameSystem Instance { get { return m_Instance; } }
+
+	void Awake()
+	{
+		m_Instance = this;
+	}
+
+	void OnDestroy()
+	{
+		m_Instance = null;
+	}
+
+	void Update()
+	{
+		// global game update logic goes here
+	}
+
+	void OnGui()
+	{
+		// common GUI code goes here
+	}
+}
