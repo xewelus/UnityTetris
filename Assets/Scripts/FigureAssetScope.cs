440151e9f6bde487eef5e2d7b55bc36b3ac06517
dinar <dinar.mobile@gmail.com>
2020-04-06 23:01:26 +0300

    Создание первой фигуры и ее спуск #2

---
+++ Assets/Scripts/FigureAssetScope.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+	[Serializable]
+	public class FigureAssetScope : MonoBehaviour
+	{
+		[DataMember]
+		public List<FigureAsset> FigureAssets = new List<FigureAsset>();
+
+		public FigureAsset GetRandom()
+		{
+			if (this.FigureAssets.Count == 0)
+			{
+				throw new Exception("this.FigureAssets.Count == 0");
+			}
+
+			int i = UnityEngine.Random.Range(0, this.FigureAssets.Count);
+			return this.FigureAssets[i];
+		}
+	}
+}
