1527a6db64eb50fa7f78196860edf96d6c3d6ff5
dinar <dinar.mobile@gmail.com>
2020-04-08 23:32:20 +0300

    GameSystem clean

--- Assets/Scripts/GameSystem.cs
+++ Assets/Scripts/GameSystem.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using Assets.Interfaces;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 namespace Assets.Scripts
 {
@@ -14,25 +13,6 @@ namespace Assets.Scripts
 		public void Awake()
 		{
 			instance = this;  
-			//this.FillCubes();
-		}
-
-		// ReSharper disable once UnusedMember.Local
-		void OnDestroy()
-		{
-			instance = null; 
-		}
-
-		// ReSharper disable once UnusedMember.Local
-		void Update()
-		{
-			// global game update logic goes here
-		}
-
-		// ReSharper disable once UnusedMember.Local
-		void OnGui()
-		{
-			// common GUI code goes here
 		}
 	}
 }
