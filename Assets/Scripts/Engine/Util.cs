19af0821b132b07d80041f5c6958b96163e0a9b1
dinar <dinar.mobile@gmail.com>
2020-04-08 22:54:17 +0300

    Util.CreateLocal

---
+++ Assets/Scripts/Engine/Util.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Engine
+{
+	public static class Util
+	{
+		public static T CreateLocal<T>(T gameObject, Transform parent, Vector3? localPosition = null) where T : Component
+		{
+			T result = Object.Instantiate(gameObject, parent);
+			if (localPosition == null)
+			{
+				localPosition = Vector3.zero;
+			}
+			result.transform.localPosition = localPosition.Value;
+			return result;
+		}
+	}
+}
