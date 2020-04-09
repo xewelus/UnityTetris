917cff0f9f35be4b15fc21cfa0601f7a212f77e9
dinar <dinar.mobile@gmail.com>
2020-04-10 00:16:09 +0300

    CompilerOptionsEditorScript

---
+++ Assets/Scripts/Engine/CompilerOptionsEditorScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scripts.Engine
+{
+	[InitializeOnLoad]
+	public class CompilerOptionsEditorScript
+	{
+		static CompilerOptionsEditorScript()
+		{
+			Debug.Log("Compiled");
+		}
+	}
+}
